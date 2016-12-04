f0f92f2645aea25ffa7f0f68eb9453a649f5b331
zhengwu <zhengwu@umich.edu>
2016-12-04 17:52:54 -0500

    add train error

--- evaluation.py
+++ evaluation.py
@@ -9,10 +9,11 @@ def classification(doc_topic, label_list, label_category_list, percentage, accur
 	classifier_list = list()
 	number_of_train = int(len(doc_topic) * percentage)
 	shuffle(label_list)
-
+	train_true_label = label_list[0: number_of_train]
 	test_true_label = label_list[number_of_train : ]
 
 	# doc -> list of label
+	train_label = defaultdict(set)
 	test_label = defaultdict(set)
 	# label list is doc -> set of label
 	for i in label_category_list:
@@ -37,6 +38,13 @@ def classification(doc_topic, label_list, label_category_list, percentage, accur
 		model = svm.SVC(kernel='linear', class_weight = 'balanced')
 		# print train_label_list
 		model.fit(X_train, train_label_list)
+
+		predict_train = model.predict(X_train)
+
+		for index in range(0, len(predict_train)):
+			if predict_train[index] == 1:
+				train_label[index].add(current_label)
+
 		predict = model.predict(X_test)
 		
 		for index in range(0, len(predict)):
@@ -48,16 +56,32 @@ def classification(doc_topic, label_list, label_category_list, percentage, accur
 		for i in range(0, len(test_label)):
 			if test_label[i] == test_true_label[i]:
 				count += 1
-		print 'tight accuracy: ' + str(float(count) / len(test_label))
-		return float(count) / len(test_label)
+		print 'test tight accuracy: ' + str(float(count) / len(test_label))
+		test_accuracy = float(count) / len(test_label)
+
+
+		count = 0
+		for i in range(0, len(train_label_list)):
+			if train_label[i] == train_true_label[i]:
+				count += 1
+		print 'train tight accuracy: ' + str(float(count) / len(train_label_list))
+
+		return test_accuracy
 	
 	elif accuracy_measure == 'loose':
 		count = 0
 		for i in range(0, len(test_label)):
 			if len(test_label[i].intersection(test_true_label[i])) >= 1:
 				count += 1
-		print 'loose accuracy: ' + str(float(count) / len(test_label))
-		return float(count) / len(test_label)
+		print 'test loose accuracy: ' + str(float(count) / len(test_label))
+		test_accuracy = float(count) / len(test_label)
+
+		count = 0
+		for i in range(0, len(train_label_list)):
+			if len(train_label[i].intersection(train_true_label[i])) >= 1:
+				count += 1
+		print 'train loose accuracy: ' + str(float(count) / len(train_label_list))
+		return test_accuracy
 	
 	else:
 		count_tight = 0
@@ -68,9 +92,24 @@ def classification(doc_topic, label_list, label_category_list, percentage, accur
 			if len(test_label[i].intersection(test_true_label[i])) >= 1:
 				count_loose += 1
 		
-		print 'tight accuracy: ' + str(float(count_tight) / len(test_label))
-		print 'loose accuracy: ' + str(float(count_loose) / len(test_label))
-		return (float(count_tight)/len(test_label), float(count_loose)/len(test_label))
+		print 'test tight accuracy: ' + str(float(count_tight) / len(test_label))
+		print 'test loose accuracy: ' + str(float(count_loose) / len(test_label))
+
+		test_tight_accuracy = float(count_tight) / len(test_label)
+		test_loose_accuracy = float(count_loose) / len(test_label)
+
+		count_tight = 0
+		count_loose = 0
+		for i in range(0, len(train_label_list)):
+			if train_label[i] == train_true_label[i]:
+				count_tight += 1
+			if len(train_label[i].intersection(train_true_label[i])) >= 1:
+				count_loose += 1
+		
+		print 'train tight accuracy: ' + str(float(count_tight) / len(train_label_list))
+		print 'train loose accuracy: ' + str(float(count_loose) / len(train_label_list))
+
+		return (test_tight_accuracy, test_loose_accuracy)
 
 if __name__ == '__main__':
 	data_file = './plsa_data'
