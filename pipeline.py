e4cfe53836f37235f995504b3aef7f53e0f5def3
XuefeiZhang <xfzhang@umich.edu>
2016-12-09 21:02:46 -0500

    network

--- pipeline.py
+++ pipeline.py
@@ -2,8 +2,8 @@ import os
 import numpy as np
 if __name__ == '__main__':
   for lambda_par in np.arange(0.1, 0.7, 0.05):
-    for gamma_par in np.arange(0.05, 0.3, 0.05):
-      print 'start lambda %f and gamma %f' % (lambda_par, gamma_par)
-      filename = 'EVALUATION/plsa_lambda%f_gamma%f' % (lambda_par, gamma_par)
-      os.system('python netplsa_with_plsa.py -rf %s -l %f -g %f' % (filename, lambda_par, gamma_par))
-      os.system('python evaluation.py -sf %s -rf %s_evaluation' % (filename, filename))
+  	for gamma_par in np.arange(0.05, 0.3, 0.05):
+  		print 'start lambda %f and gamma %f' % (lambda_par, gamma_par)
+  		filename = 'EVALUATION/plsa_lambda%f_gamma%f' % (lambda_par, gamma_par)
+  		os.system('python netplsa_with_plsa.py -rf %s -l %f -g %f' % (filename, lambda_par, gamma_par))
+  		os.system('python evaluation.py -sf %s -rf %s_evaluation' % (filename, filename))
