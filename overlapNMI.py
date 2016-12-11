32c6aba80a8628602c83bbf79475ad82e886a74c
XuefeiZhang <xfzhang@umich.edu>
2016-12-11 00:31:29 -0500

    overlapNMI modified

--- overlapNMI.py
+++ overlapNMI.py
@@ -32,16 +32,11 @@ def overlapNMI(cluster1, cluster2):
     sum2 = -((B+D)*np.log2(BDtemp/n) + (A+C)*np.log2(ACtemp/n))
     H_X_Y_mat = sum1-sum2
     
-    sum3 = -((A+B)*np.log2(ABtemp/n) + (C+D)*np.log2(CDtemp/n))
+    #sum3 = -((A+B)*np.log2(ABtemp/n) + (C+D)*np.log2(CDtemp/n))
     
-    index = -(A*np.log2(Atemp/n) + D*np.log(Dtemp/n)) + (B*np.log2(Btemp/n) + C*np.log2(Ctemp/n))
-    H_star_X_Y = np.array(H_X_Y_mat)
-    H_star_X_Y[index < 0] = sum3[index < 0 ]
-    
-    H_X = np.min(H_star_X_Y, axis = 0)
-    H_X_Y = sum(H_X)
-    H_Y = np.min(H_star_X_Y, axis = 1)
-    H_Y_X = sum(H_Y)
+    #index = -(A*np.log2(Atemp/n) + D*np.log(Dtemp/n)) + (B*np.log2(Btemp/n) + C*np.log2(Ctemp/n))
+    #H_star_X_Y = np.array(H_X_Y_mat)
+    #H_star_X_Y[index < 0] = sum3[index < 0 ]
     
     X_marginal_1 = np.sum(cluster1, axis = 0)
     X_marginal_0 = np.sum(1-cluster1, axis = 0)
@@ -57,12 +52,32 @@ def overlapNMI(cluster1, cluster2):
     Y_marginal_1_temp[Y_marginal_1 == 0] = n
     Y_marginal_0_temp = np.array(Y_marginal_0)
     Y_marginal_0_temp[Y_marginal_0 == 0] = n
+
+
+    H_X = np.min(H_X_Y_mat, axis = 0)
+    H_X_marginal = X_marginal_1 * np.log2(X_marginal_1_temp/n) + \
+    X_marginal_0 * np.log2(X_marginal_0_temp/n)
+    H_X_Y_norm = H_X/H_X_marginal
+    H_X_Y = np.sum(H_X_Y_norm)/K
     
-    H_X_marginal = -np.sum(X_marginal_1 * np.log2(X_marginal_1_temp/n) + \
-    X_marginal_0 * np.log2(X_marginal_0_temp/n))
+    H_Y = np.min(H_X_Y_mat, axis = 1)
+
+    #H_X_Y = sum(H_X)
     
-    H_Y_marginal = -np.sum(Y_marginal_1 * np.log2(Y_marginal_1_temp/n) + \
-    Y_marginal_0 * np.log2(Y_marginal_0_temp/n))
+    H_Y_X = sum(H_Y)
+    H_Y_marginal = Y_marginal_1 * np.log2(Y_marginal_1_temp/n) + \
+    Y_marginal_0 * np.log2(Y_marginal_0_temp/n)
+    H_Y_X_norm = H_Y/H_Y_marginal
+    H_Y_X = np.sum(H_Y_X_norm)/K
+
+    NMI = 1 - 1/2(H_Y_X + H_X_Y)
+ 
+    # H_X_marginal = -np.sum(X_marginal_1 * np.log2(X_marginal_1_temp/n) + \
+    # X_marginal_0 * np.log2(X_marginal_0_temp/n))
+    
+    # H_Y_marginal = -np.sum(Y_marginal_1 * np.log2(Y_marginal_1_temp/n) + \
+    # Y_marginal_0 * np.log2(Y_marginal_0_temp/n))
+
     
     NMI = 1 - 0.5*(H_X_Y/H_X_marginal + H_Y_X/H_Y_marginal)
     print 'The NMI of the overlapping community detection is' + str(NMI)
